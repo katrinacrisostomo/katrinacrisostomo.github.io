b504672d1cc3542898a62619666c4341bcede7e9
katrinacrisostomo <katrinacrisostomo@users.noreply.github.com>
2026-05-07 13:36:24 -0400

    Revert "switch router?"
    This reverts commit 3afec162d1dfb97a6a781f3eef2ee8d56c65bea5.

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
-// Support GitHub Pages deep links via `public/404.html`.
-const pendingRedirect = sessionStorage.getItem("spa:redirect");
-if (pendingRedirect) {
-  sessionStorage.removeItem("spa:redirect");
-  window.history.replaceState(null, "", pendingRedirect);
-}
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
